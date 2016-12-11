36a78f645557c109c93537a02917c0bf72f4c68e
John Schoeman <johnschoeman1617@gmail.com>
2016-12-11 18:02:31 -0500

    add a Static Pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
-  def home
+  def calculator
   end
 
   def help
+  end
+
+  def about
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get calculator" do
+    get static_pages_calculator_url
     assert_response :success
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
