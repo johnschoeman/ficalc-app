973f84738ad08c296d7c36bec80d8eb0ba5d8226
John Schoeman <johnschoeman1617@gmail.com>
2016-12-17 20:40:31 -0500

    finish ficalc controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
+  root 'fi_calc#timetofi'
   get 'fi_calc/timetofi'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
--- test/controllers/fi_calc_controller_test.rb
+++ test/controllers/fi_calc_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class FiCalcControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get timetofi" do
     get fi_calc_timetofi_url
     assert_response :success
+    assert_select "title", "TimeToFi | FICalc App"
   end
 
 end
