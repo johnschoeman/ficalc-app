856689a183d82a81c147204c95c7044cf3d73f64
John Schoeman <johnschoeman1617@gmail.com>
2016-12-27 20:08:22 -0500

    refactor ficalculator.js

--- app/assets/javascripts/ficalculator.js
+++ app/assets/javascripts/ficalculator.js
@@ -1,69 +1,16 @@
-function getMonthSpending()
-{
-    var monthSpending=0;
-    //Get a reference to the form id="timetofiform"
-    var theForm = document.forms["timetofiform"];
-    //Get a reference to the select id="MonthSpending"
-    var monthSpendingInput = theForm.elements["monthspending"];
-    monthSpending = monthSpendingInput.value;
-    return parseInt(monthSpending);
-}
-
-function getMonthIncome()
-{
-    var monthIncome=0;
-    var theForm = document.forms["timetofiform"];
-    var monthIncomeInput = theForm.elements["monthincome"];
-    monthIncome = monthIncomeInput.value;
-    return parseInt(monthIncome);
-}
-
-function getNetWorth()
-{
-    var netWorth=0;
-    var theForm = document.forms["timetofiform"];
-    var netWorthInput = theForm.elements["networth"];
-    netWorth = netWorthInput.value;
-    return parseInt(netWorth);
-}
-
-function getAverageReturn()
-{
-    var averageReturn=0;
-    var theForm = document.forms["timetofiform"];
-     var averageReturnInput = theForm.elements["averagereturn"];
-    averageReturn = averageReturnInput.value;
-    return parseInt(averageReturn);
-}
-
-function getInflation()
-{
-    var inflation=0;
-    var theForm = document.forms["timetofiform"];
-    var inflationInput = theForm.elements["inflation"];
-    inflation = inflationInput.value;
-    return parseInt(inflation);
-}
-
-function getRaises()
-{
-    var raises=0;
-    var theForm = document.forms["timetofiform"];
-    var raisesInput = theForm.elements["raises"];
-    raises = raisesInput.value;
-    return parseInt(raises);
-}
-
+function getUserInput(input) {
+  return parseInt(document.forms["timetofiform"].elements[input].value);
+};
 
 function calculateFI()
 {
     var monthCount = 0;
-    var netWorth = getNetWorth();
-    var monthlySpending = getMonthSpending();
-    var monthlyIncome = getMonthIncome();
-    var averageReturn = getAverageReturn()/100 + 1;
-    var inflation = getInflation()/100 + 1;
-    var raises = getRaises()/100 + 1;
+    var netWorth = getUserInput("networth");
+    var monthlySpending = getUserInput("monthspending");
+    var monthlyIncome = getUserInput("monthincome");
+    var averageReturn = getUserInput("averagereturn")/100 + 1;
+    var inflation = getUserInput("inflation")/100 + 1;
+    var raises = getUserInput("raises")/100 + 1;
 
     var percentFI = (netWorth / (300 * monthlySpending));
 
@@ -86,7 +33,7 @@ function calculateFI()
 
 function test()
 {
-  var result = getInflation();
+  var result = getUserInput("monthspending");
 
   var divobj = document.getElementById('demo');
   divobj.style.display='block';
