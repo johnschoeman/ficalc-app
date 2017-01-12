c584981c75e5fb98ce53bbd0a26bd7f11511229d
John Schoeman <johnschoeman1617@gmail.com>
2017-01-11 21:38:58 -0500

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'fi_calc#timetofi'
-  get '/glossary', to: 'fi_calc#glossary'
+  get '/glossary',  to: 'fi_calc#glossary'
+  get '/about',     to: 'fi_calc#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
