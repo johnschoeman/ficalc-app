b10cf3e07b5c67169bcdc6f0982d52451faca3d2
John Schoeman <johnschoeman1617@gmail.com>
2017-01-17 22:51:57 -0500

    Add contact page with email

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'fi_calc#timetofi'
   get '/glossary',  to: 'fi_calc#glossary'
   get '/about',     to: 'fi_calc#about'
+  get '/contact',   to: 'fi_calc#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
