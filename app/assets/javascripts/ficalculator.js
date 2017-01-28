9e3340e0461cc551b26a01caef6cb43d5f55053e
John Schoeman <johnschoeman1617@gmail.com>
2017-01-27 22:10:04 -0500

    add mario

--- app/assets/javascripts/ficalculator.js
+++ app/assets/javascripts/ficalculator.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
   updateDisplay();
+  $(document).keydown(function(key) {
+      switch(parseInt(key.which,10)) {
+    // Left arrow key pressed
+    case 37:
+      $('#mario').animate({left: "-=10px"}, 'fast');
+      break;
+    // Up Arrow Pressed
+    case 38:
+      // Put our code here
+      $('#mario').animate({top: "-=10px"}, 'fast');
+      break;
+    // Right Arrow Pressed
+    case 39:
+      // Put our code here
+      $('#mario').animate({left: "+=10px"}, 'fast');
+      break;
+    // Down Arrow Pressed
+    case 40:
+      // Put our code here
+      $('#mario').animate({top: "+=10px"}, 'fast');
+      break;
+    };
+  });
 });
 
 //Display Functions to update html page.
@@ -212,7 +235,8 @@ function displayTimeToFIChart(dataArray) {
       curveType: 'function',
       legend: { position: 'bottom' },
       colors: ['#4286f4', 'e02828'],
-      vAxis: {format: 'currency'}
+      vAxis: {format: 'currency'},
+      lineWidth: 3
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('chart'));
