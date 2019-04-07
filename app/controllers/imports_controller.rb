74e2fb41eed3c171d8cb71cc94d49649cb057962
John Schoeman <johnschoeman1617@gmail.com>
2019-04-07 14:31:51 -0400

    Allow for users to upload either xlsx or csv
    Why:
    We would like for users to be able to upload either csv or xlsx files
    for their data
    
    This commit:
    Introduces the import functionality to the importer class with the gem
    rubyXL to handle the xlsx parsing.

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,6 +1,6 @@
 class ImportsController < ApplicationController
   def create
-    FinancialDataImporter.import(params[:file].path, current_user)
+    FinancialDataImporter.import(params[:file].path, current_user.id)
     redirect_to financial_data_path, notice: "Succesfully imported"
   end
 end
