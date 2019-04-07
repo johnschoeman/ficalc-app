d26c4f4df678adc1276f2306514db680b409bcce
John Schoeman <johnschoeman1617@gmail.com>
2019-04-07 11:41:49 -0400

    Update formatter to handle currency format
    Why:
    We would like for the formatter to be able to intake data that is either
    an integer a float, or a currency string and return a float.
    
    This commit:
    Adds the conversion of data inputs into a float.

--- spec/models/csv_importer_spec.rb
+++ spec/models/csv_importer_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
+require "csv"
 
 RSpec.describe "CsvImporter" do
   describe ".import" do
     it "creates financial data for the rows" do
-      require "csv"
-      user = create(:user, id: 1)
+      user = create(:user)
       file =
         CSV.generate do |csv|
           csv << %w[month year income expenses net_worth]
@@ -24,6 +24,35 @@ RSpec.describe "CsvImporter" do
       importer.import
 
       expect(FinancialDatum.count).to eq 3
+    end
+
+    context "the csv has non-integer data" do
+      it "creates the data for the rows" do
+        user = create(:user)
+        file =
+          CSV.generate do |csv|
+            csv << %w[month year income expenses net_worth]
+            csv << %w[3 2019 $1,000 $500 $1,0000]
+            csv << %w[4 2019 $1,000 $500 $1,0000]
+            csv << %w[5 2019 $1,000 $500 $1,0000]
+          end
+        filename = "test.csv"
+        allow(File).to receive(:open).with(
+          filename,
+          "r",
+          { headers: true, universal_newline: false },
+        )
+          .and_return(file)
+        importer = CsvImporter.new(filename, user.id)
+
+        importer.import
+
+        expect(FinancialDatum.count).to eq 3
+        last_datum = FinancialDatum.last
+        expect(last_datum.income).to eq(1000)
+        expect(last_datum.expenses).to eq(500)
+        expect(last_datum.net_worth).to eq(10000)
+      end
     end
   end
 end
