b3e8dacad21e5946998c219c03370e8a7cc5ba12
John Schoeman <johnschoeman1617@gmail.com>
2019-04-07 16:44:56 -0400

    Fix import worker spec

--- spec/workers/financial_data_worker_spec.rb
+++ spec/workers/financial_data_worker_spec.rb
@@ -5,17 +5,14 @@ RSpec.describe FinancialDataImportWorker do
     it "fetches all the files from the ftp server" do
       file_path = "data/sample.csv"
       user_id = 1
-      importer_double = double("importer")
-      allow(importer_double).to receive(:import)
-      allow(FinancialDataImporter).to receive(:new).and_return(importer_double)
+      allow(FinancialDataImporter).to receive(:import)
 
       FinancialDataImportWorker.new.perform(file_path, user_id)
 
-      expect(FinancialDataImporter).to have_received(:new).with(
+      expect(FinancialDataImporter).to have_received(:import).with(
         file_path,
         user_id,
       )
-      expect(importer_double).to have_received(:import)
     end
   end
 end
