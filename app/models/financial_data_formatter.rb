6cd3fb2f94986b94292e7f15fbf147f6157d4e97
John Schoeman <johnschoeman1617@gmail.com>
2019-04-07 12:11:27 -0400

    Update formatter to handle multiple dates
    Why:
    We would like for the formatter to be able to handle dates of both the
    yyyymmdd format and the yyyy-mm-dd format
    
    this commit:
    updates the formatter to handle both date formats.

--- app/models/financial_data_formatter.rb
+++ app/models/financial_data_formatter.rb
@@ -3,7 +3,7 @@ class FinancialDataFormatter
     raw_data.keys.each do |key|
       case key
       when :month
-        raw_data[:month] = FinancialDatum::MONTHS[raw_data[:month].to_i]
+        raw_data[:month] = get_month(raw_data[:month])
       when :expenses
         raw_data[:expenses] = convert_to_integer(raw_data[:expenses])
       when :income
@@ -11,6 +11,11 @@ class FinancialDataFormatter
       when :net_worth
         raw_data[:net_worth] = convert_to_integer(raw_data[:net_worth])
       when :year
+      when :date
+        parsed_date = parse_date(raw_data[:date])
+        raw_data[:month] = get_month(parsed_date.month - 1)
+        raw_data[:year] = parsed_date.year
+        raw_data.delete(:date)
       else
         raise "Unknown column: #{key}"
       end
@@ -21,7 +26,21 @@ class FinancialDataFormatter
 
   private
 
+  def get_month(integer)
+    FinancialDatum::MONTHS[integer.to_i]
+  end
+
   def convert_to_integer(value)
     value.class == String ? value.gsub("$", "").gsub(",", "").to_i : value.to_f
+  end
+
+  def parse_date(date)
+    if /\d{8}/.match?(date)
+      Date.strptime("#{date[0..3]}-#{date[4..5]}-#{date[6..7]}")
+    elsif /\d{4}-\d{2}-\d{2}/.match?(date)
+      Date.strptime(date)
+    else
+      raise "Unknown date format: #{date}"
+    end
   end
 end
